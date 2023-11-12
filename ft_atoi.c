faae7c25f270b7a3195f35714ddbd1eb4a315a7b
Hewe4 <naderu3010@gmail.com>
2023-11-12 18:26:18 +0100

    memcmp terminado y atoi arreglado

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,33 +6,41 @@
 /*   By: ide-ruit <ide-ruit@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 20:32:58 by ide-ruit          #+#    #+#             */
-/*   Updated: 2023/09/19 17:52:15 by ide-ruit         ###   ########.fr       */
+/*   Updated: 2023/11/12 18:22:04 by ide-ruit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *nb)
-{
-	int	i;
-	int	s;
-	int	n;
+int ft_atoi(const char *nb) {
+    int i;
+    int s;
+    int n;
+    int d;
 
-	i = 0;
-	s = 1;
-	n = 0;
-	while (nb[i] == '+' || nb[i] == '-')
-	{
-		if (nb[i] == '-')
-		{
-			s *= -1;
-		}
-		i++;
-	}
-	while (nb[i] <= '9' && nb[i] >= '0')
-	{
-		n = (n * 10) + (nb[i] - '0');
-		i++;
-	}
-	return (n * s);
+    i = 0;
+    s = 1;
+    n = 0;
+	d = 0;
+    while (nb[i] == ' ' || (nb[i] >= '\t' && nb[i] <= '\r'))
+        i++;
+    while (nb[i] == '+' || nb[i] == '-') {
+        if (nb[i] == '-')
+            s *= -1;
+        i++;
+    }
+    while (nb[i] >= '0' && nb[i] <= '9') {
+        d = nb[i] - '0';
+
+        if (n > (2147483647 - d) / 10) {
+            if (s == 1)
+                return 2147483647;
+            else
+                return -2147483648;
+        }
+
+        n = n * 10 + d;
+        i++;
+    }
+    return n * s;
 }
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ide-ruit <ide-ruit@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 16:27:35 by ide-ruit          #+#    #+#             */
-/*   Updated: 2023/11/12 16:42:16 by ide-ruit         ###   ########.fr       */
+/*   Updated: 2023/11/12 18:04:21 by ide-ruit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,21 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char *a;
 	unsigned char *b;
+	int	i;
+	int	x;
+
+	i = 0;
+	x = 0;
+	a = s1;
+	b = s2;
+	while(i < n)
+	{
+		x = a[i] - b[i];
+		if(x != 0)
+		{
+			return x;
+		}
+		i++;
+	}
+	return x;
 }
