d1d5d4f093f20adb1863e571ba51fed815ae56a9
Hewe4 <naderu3010@gmail.com>
2024-02-10 18:54:29 +0100

    atoi arreglado

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,41 +6,36 @@
 /*   By: ide-ruit <ide-ruit@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 20:32:58 by ide-ruit          #+#    #+#             */
-/*   Updated: 2023/11/12 18:22:04 by ide-ruit         ###   ########.fr       */
+/*   Updated: 2024/02/10 18:50:40 by ide-ruit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *nb) {
-    int i;
-    int s;
-    int n;
-    int d;
+int	ft_atoi(const char *nb)
+{
+	int	i;
+	int	sign;
+	int	number;
+	int	d;
 
-    i = 0;
-    s = 1;
-    n = 0;
+	i = 0;
+	sign = 1;
+	number = 0;
 	d = 0;
-    while (nb[i] == ' ' || (nb[i] >= '\t' && nb[i] <= '\r'))
-        i++;
-    while (nb[i] == '+' || nb[i] == '-') {
-        if (nb[i] == '-')
-            s *= -1;
-        i++;
-    }
-    while (nb[i] >= '0' && nb[i] <= '9') {
-        d = nb[i] - '0';
-
-        if (n > (2147483647 - d) / 10) {
-            if (s == 1)
-                return 2147483647;
-            else
-                return -2147483648;
-        }
-
-        n = n * 10 + d;
-        i++;
-    }
-    return n * s;
+	while (nb[i] == ' ' || (nb[i] >= '\t' && nb[i] <= '\r'))
+		i++;
+	while (nb[i] == '+' || nb[i] == '-')
+	{
+		if (nb[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nb[i] >= '0' && nb[i] <= '9')
+	{
+		d = nb[i] - '0';
+		number = number * 10 + d;
+		i++;
+	}
+	return (number * sign);
 }
