cb919165ee25c8793cb4970849f9eda7ced8c4b9
Hewe4 <naderu3010@gmail.com>
2024-02-10 17:30:43 +0100

    calloc arreglado

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ide-ruit <ide-ruit@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 19:54:51 by ide-ruit          #+#    #+#             */
-/*   Updated: 2024/02/10 18:28:02 by ide-ruit         ###   ########.fr       */
+/*   Updated: 2024/02/10 17:30:30 by ide-ruit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	*ft_calloc(size_t ne, size_t te)
 	call = malloc(ne * te);
 	if(!call)
 	return(NULL);
-	ft_bzero.c(call,(ne * te));
+	ft_bzero(call,(ne * te));
 	return (call);
 }
