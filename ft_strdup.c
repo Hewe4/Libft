ef5124d782fc610d84c794a0174fa3b356bf5c6d
Hewe4 <naderu3010@gmail.com>
2024-02-10 18:13:55 +0100

    añadido strdup

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ide-ruit <ide-ruit@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 17:57:59 by ide-ruit          #+#    #+#             */
+/*   Updated: 2024/02/10 17:58:00 by ide-ruit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*p;
+	size_t	l;
+	int		i;
+
+	i = 0;
+	l = ft_strlen(s);
+	p = (char *)malloc(sizeof(char) * (len + 1));
+	if(!p)
+	return(NULL);
+	while(s[i] != '\0')
+	{
+		p[i] = s[i];
+		i++;
+	}
+	p[i] = "\0";
+	return (p);
+}
